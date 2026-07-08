c94bd90e8bf5eaf48cb86f9f81d3606d21aa4e4b
LogaritmoRosa <dmarotox@gmail.com>
2026-07-08 14:01:28 +0200

    feat(setup): initialize clean project

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <SFML/Graphics.hpp>
+#include <optional>
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "Pathfinding Visualizer");
+    while (window.isOpen()) {
+        while (const std::optional<sf::Event> event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>()) {
+                window.close();
+            }
+        }
+        window.clear(sf::Color::Black);
+        window.display();
+    }
+    return 0;
+}
